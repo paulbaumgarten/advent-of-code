22885b5e175d4812a034566a79177574eb0e23ce
Paul Baumgarten <pbaumgarten@gmail.com>
2023-12-12 22:43:03 +0800

    gr

--- 2023/2023-12.py
+++ 2023/2023-12.py
@@ -10,6 +10,11 @@ TEST = """???.### 1,1,3
 # part a 21
 # part b 525152
 
+TEST2 = """??.?? 1,1
+??.??.??.?? 1,1,1,1
+??.??#??.?? 1,1,1,1
+??.?????.?? 1,1,1,1""".splitlines()
+
 def part1():
     with open(FILE, "r") as f:
         data = f.read().splitlines()
@@ -50,12 +55,7 @@ def count_patterns_match_target(pattern, target):
     original = pattern
     arrangements_this_pattern = 0
     unknowns = pattern.count("?")
-    #while pattern.count("..") > 0:
-    #    pattern = pattern.replace("..",".")
-    #if pattern.startswith("."):
-    #    pattern = pattern[1:]
-    #if pattern.endswith("."):
-    #    pattern = pattern[:-1]
+    matches = 0
     for i in range(0, 2**unknowns):
         n = i
         this_arrangement = pattern
@@ -63,30 +63,33 @@ def count_patterns_match_target(pattern, target):
             new_char = "#" if n % 2 == 0 else "."
             n = n // 2
             this_arrangement = this_arrangement.replace("?", new_char, 1)
-            #if this_arrangement.startswith("."):
-            #    this_arrangement = this_arrangement[1:]
-            #if this_arrangement.endswith("."):
-            #    this_arrangement = this_arrangement[:-1]
-        # Does this arrangement match the target
+        while this_arrangement.count("..") > 0:
+            this_arrangement = this_arrangement.replace("..",".")
+        if this_arrangement.startswith("."):
+            this_arrangement = this_arrangement[1:]
+        if this_arrangement.endswith("."):
+            this_arrangement = this_arrangement[:-1]
         if this_arrangement == target:
-            print(" -> ",this_arrangement)
-            arrangements_this_pattern += 1
-    return arrangements_this_pattern
+            matches += 1
+    return matches
 
 def part2():
     with open(FILE, "r") as f:
         data = f.read().splitlines()
-    data = TEST
+    data = TEST2
     total = 0
-    #for r in range(0, len(data)):
-    for r in range(1, 2):
+    for r in range(0, len(data)):
+    #for r in range(1, 2):
         pattern, counts = data[r].split(" ")
         # Unfold the data .... 5 copies! eek
-        pattern = ((pattern + "?") * 5)[:-1]
-        simple_pattern = pattern + "."
-        while simple_pattern.count("..") > 0:
-            simple_pattern = simple_pattern.replace("..",".")
-        counts = ((counts + ",") * 5)[:-1]
+        pre_pattern = "?"+pattern
+        post_pattern = pattern+"?"
+        orig_pattern = pattern
+        #pattern = ((pattern + "?") * 5)[:-1]
+        #simple_pattern = pattern + "."
+        #while simple_pattern.count("..") > 0:
+        #    simple_pattern = simple_pattern.replace("..",".")
+        #counts = ((counts + ",") * 5)[:-1]
         # Now let's work        
         counts = [int(n) for n in counts.split(",")]
         # What is the target patten?
@@ -95,21 +98,24 @@ def part2():
             target += "#"*counts[k]
             if k < len(counts)-1:
                 target += "."
-        print(pattern, counts)
-        print(simple_pattern)
-        print(target)
+        print(pattern, counts, target)
+        #print(simple_pattern)
+        #print(target)
         # Calculate the permuations
-        consecutive_blocks = 0
-        unknown_blocks = 0
-        for c in range(0, len(simple_pattern)):
-            if simple_pattern[c] == ".":
-                consecutive_blocks = 0
-            elif simple_pattern[c] == "#":
-                consecutive_blocks += 1
-            elif simple_pattern[c] == "?":
-                unknown_blocks += 1
+        matches_this_pattern = count_patterns_match_target(pattern, target)
+        print(matches_this_pattern)
+        matches_this_pattern_pre = count_patterns_match_target(pre_pattern, target)
+        print(matches_this_pattern_pre)
+        matches_this_pattern_post = count_patterns_match_target(post_pattern, target)
+        print(matches_this_pattern_post)
+        #if matches_this_pattern==matches_this_pattern_post or matches_this_pattern==matches_this_pattern_pre: # the additional '?' must be used as a '.'
+        #    total += matches_this_pattern**5
+        #else:
+        total += max(matches_this_pattern_pre,matches_this_pattern_post)**4 * matches_this_pattern
     print(total)
     
 #part1()
 part2()
+# 316055368812 is too low
+
 
